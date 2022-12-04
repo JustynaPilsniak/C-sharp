1a14ad00930b223370d4e264340e36e19060b28e
JustynaPilsniak <117394351+JustynaPilsniak@users.noreply.github.com>
2022-12-04 21:45:37 +0100

    update pole trojkata

--- zadanie domowe/zadanie domowe/Program.cs
+++ zadanie domowe/zadanie domowe/Program.cs
@@ -11,26 +11,45 @@ namespace zadanie_domowe
         static void Main(string[] args)
         {
             //oblicz pole kwadratu, pole prostokąta, obwód kwadratu, obwód prostokąta, zastosowanie console writeline, na końcu console readline
-            int a = 6;
-            Console.WriteLine("bok jest równy:" + a);
+            //int a = 6;
+            //Console.WriteLine("bok jest równy:" + a);
 
-            int poleKwadratu = a * a;
-            Console.WriteLine("Pole kwadratu wynosi:" + poleKwadratu);
+            //int poleKwadratu = a * a;
+            //Console.WriteLine("Pole kwadratu wynosi:" + poleKwadratu);
 
-            int obwodKwadratu = a + a + a + a;
-            Console.WriteLine("Obwod kwadratu wynosi:" + obwodKwadratu);
+            //int obwodKwadratu = a + a + a + a;
+            //Console.WriteLine("Obwod kwadratu wynosi:" + obwodKwadratu);
 
-            int b = 7;
-            Console.WriteLine("pierwszy bok jest równy:" + b);
+            //int b = 7;
+            //Console.WriteLine("pierwszy bok jest równy:" + b);
 
-            int c= 5;
-            Console.WriteLine("drugi bok jest równy:" + c);
+            //int c= 5;
+            //Console.WriteLine("drugi bok jest równy:" + c);
 
-            int poleProstokata = b * c;
-            Console.WriteLine("pole prostokata wynosi:" + poleProstokata);
+            //int poleProstokata = b * c;
+            //Console.WriteLine("pole prostokata wynosi:" + poleProstokata);
 
-            int obwodProstokata = b + b + c + c;
-            Console.WriteLine("Obwod kwadratu wynosi:" + obwodProstokata);
+            //int obwodProstokata = b + b + c + c;
+            //Console.WriteLine("Obwod kwadratu wynosi:" + obwodProstokata);
+
+            //Oblicz pole trojkata jesli bok a wynosi 2,5, a wysokosc h wynosi 2.
+
+
+            Console.WriteLine("Ile wynosi pole trójkąta?");
+
+            double a = 2.5;
+            Console.WriteLine("bok a jest równy: " + a);
+
+            double h = 2.0;
+            Console.WriteLine("wysokość jest równa: " + h);
+
+
+            double poleTrojkata = 0.5 * 2.5 * 2.0;
+            Console.WriteLine("Pole trojkata wynosi: " + poleTrojkata); 
+
+
+
+
 
             Console.ReadLine();
 
